31bdbee7828b90d65b89da6bb680494d6a32144a
MurCoda LLC <murcoda@ro.ru>
2026-04-24 21:02:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+document.getElementById('content').style.display = 'none';
+document.getElementById('loading').style.display = 'block';
+setTimeout(function() {
+  document.getElementById('content').style.display = 'block';
+  document.getElementById('loading').style.display = 'none';
+}, 4000);
