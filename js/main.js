7c4d1f23b6665ddb7bb5f6623cd454de29dc9772
MurCoda LLC <murcoda@ro.ru>
2026-05-20 20:11:26 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
-
+function body() {
+  document.getElementById("nobody").innerHTML = "<header><a href=\"/\"><video id=\"logo\" src=\"/assets/lv_0_20260427192734.mp4\" loop autoplay muted ></video></a><h1>MurCoda</h1></header>";
+body()
